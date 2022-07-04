6610f21bcbb37a313485e9f689cf0b74700e33ae
Jerry092383 <jerry092383@mail.chungyo.net>
2022-07-04 11:48:19 +0800

    init tool

---
+++ index.ts
@@ -0,0 +1,5 @@
+import tool from './utils/tool';
+
+export {
+    tool
+};
---
+++ utils/tool.ts
@@ -0,0 +1,21 @@
+function toAsync<T>(promiseObject: Promise<T>): Promise<{
+    status: boolean;
+    response?: T,
+    error?: any
+}> {
+    return new Promise(resolve => {
+        promiseObject.then(res => {
+            return resolve({
+                status: true,
+                response: res
+            });
+        }).catch(err => {
+            return resolve({
+                status: false,
+                error: err
+            })
+        });
+    });
+}
+
+export default toAsync;
